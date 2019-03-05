e67c264e8f0f0c1c8da5f6fbb108696db9dd116a
mgnush <mhjorthen@gmail.com>
2019-03-06 00:28:08 +1100

    Implement three direction schemes each with their own function to be called from main. Parent window direction works well with the two moving away recordings, but gives wrong result by big margin on approaching recordings.

--- Main.cpp
+++ Main.cpp
@@ -19,8 +19,8 @@
 const double DOPPLER_MIN = 0.8491;
 const double DOPPLER_MAX = 1.0425;
 // Define doppler-adjusted frequency band of interest
-const double threshLow = 700 * DOPPLER_MAX;
-const double threshHigh = 1500 * DOPPLER_MIN;
+const double BAND_FREQ_MIN = 700;
+const double BAND_FREQ_MAX = 1550;
 // Frequencies for multithresholding
 const double noiseThreshLowMin = 150;
 const double noiseThreshLowMax = 510;
@@ -36,6 +36,7 @@ const int W = 2; // Number of windows to keep
 const int SPLIT = 2;
 const double subWindow = fullWindow / SPLIT;
 const int SW = SPLIT * W;
+const double dirMargin = 0.015; // Changes below this magnitude (%) are considered to be inconclusive
 
 struct rec_data {
 	int channels;
@@ -44,7 +45,7 @@ struct rec_data {
 	double *samples;
 };
 
-struct multiThresholdIndeces {
+struct multi_thresh_indeces {
 	int bandIndeces[BANDS + 1];
 	int bandLength;
 	int noiseIndexLowMin;
@@ -53,18 +54,17 @@ struct multiThresholdIndeces {
 	int noiseIndexHighMax;
 };
 
-struct fftVars {
+struct fft_vars {
 	double *window;
 	fftw_complex *out;   
 	fftw_plan p;
 	double *absFFT;
 };
 
-struct fftAnalysis {
+struct fft_analysis {
 	double bandAvgs[BANDS];
 	double noiseThresh;
 };
-
 
 void fftToFile(const char* fileName, fftw_complex *&data)
 {
@@ -110,9 +110,14 @@ rec_data readRecording(const char* fileName) {
 	return data;
 }
 
-multiThresholdIndeces setupMultiThresholding(const int &n, const int &fs) {
+multi_thresh_indeces setupMultiThresholding(const int &n, const int &fs, const bool &doppler) {
 
-	multiThresholdIndeces mtIndeces;
+	multi_thresh_indeces mtIndeces;
+
+	double threshLow = BAND_FREQ_MIN * (1 + (doppler * (DOPPLER_MAX - 1)));
+	double threshHigh = BAND_FREQ_MAX * (1 + (doppler * (DOPPLER_MIN - 1)));
+	// TESTING ONLY
+	printf("The band minimum is %.1f and the maximum is %.1f \n", threshLow, threshHigh);
 
 	// Find array indeces
 	double df = (double)fs / (double)n;
@@ -131,8 +136,8 @@ multiThresholdIndeces setupMultiThresholding(const int &n, const int &fs) {
 	return mtIndeces;
 }
 
-fftVars setupFFT(const int &nWindow) {
-	fftVars vars;
+fft_vars setupFFT(const int &nWindow) {
+	fft_vars vars;
 	vars.window = (double*)malloc(sizeof(double) * 2 * (nWindow / 2 - 1));
 	vars.out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * 2 * (nWindow / 2 - 1));   // In-place fft requires in and out to accomodate two out-arrays   // Complex 1D, n/2-1 length output
 	vars.p = fftw_plan_dft_r2c_1d(nWindow, vars.window, vars.out, FFTW_ESTIMATE); // MEASURE consumes extra time on initial plan execution. ESTIMATE has no initial timecost.
@@ -141,8 +146,9 @@ fftVars setupFFT(const int &nWindow) {
 	return vars;
 }
 
-fftAnalysis doFFT(fftVars vars, const rec_data &rec, const multiThresholdIndeces &mtIndeces, const int &nWindow, int i) {
-	fftAnalysis fftAnal;
+fft_analysis doFFT(fft_vars vars, const rec_data &rec, const multi_thresh_indeces &mtIndeces, const int &nWindow, int i) {
+	fft_analysis fftAnal;
+
 	// SPLIT up sound file
 	for (int j = 0; j < rec.channels * nWindow; j += rec.channels) {
 		vars.window[j >> (rec.channels >> 1)] = rec.samples[i*nWindow + j]; // Ignore all channels but channel 0 NB: CANT HAVE UNEVEN NUMBER OF CHANNELS
@@ -185,99 +191,140 @@ fftAnalysis doFFT(fftVars vars, const rec_data &rec, const multiThresholdIndeces
 	return fftAnal;
 }
 
-int *detect(const fftAnalysis &fftAnal) {
-	int detectedBands[BANDS] = { 0 };
+
+void detect(const fft_analysis &fftAnal, int(&detectedBands)[BANDS]) {
+	int detections = 0;
+
 	for (int i = 0; i < BANDS; i++) {
 		detectedBands[i] = (fftAnal.bandAvgs[i] >= fftAnal.noiseThresh);
 	}
-	return detectedBands;
+	for (int i = 0; i < BANDS; i++)	{
+		detections += detectedBands[i];
+	}
+	printf("The siren is present in %d out of %d bands, and they are:", detections, BANDS);
+
+	for (int j = 0; j < BANDS; j++)	{
+		printf(" %d ", detectedBands[j]);
+	}
+	printf("\n");
+}
+
+// Compare only parent windows
+void directionParentOnly(const fft_analysis *fftAnals, const int (&detectedBands)[W][BANDS]) {
+	double windowAvgs[W] = { 0 };
+
+	for (int i = 0; i < W; i++) {
+		for (int j = 0; j < BANDS; j++) {
+			windowAvgs[i] += (fftAnals[i].bandAvgs[j] * detectedBands[i][j]);
+		}
+		windowAvgs[i] = windowAvgs[i] / BANDS;
+	}
+
+	for (int i = 1; i < W; i++) {
+		double rel = windowAvgs[i] / windowAvgs[i - 1];
+		if (rel > (1 + dirMargin)) {
+			printf("Detected EV is approaching at %f. \n", rel);
+		}
+		else if (rel < (1 - dirMargin)) {
+			printf("Detected EV is moving away at %f. \n", rel);
+		}
+		else {
+			printf("Detected direction is inconclusive at %f. \n", rel);
+		}
+	}
+}
+
+// Compare all subwindows but only in parent detected bands
+void directionSubParentBands(const fft_analysis *fftSubs, const int *detectedBands, const int &i) {
+	double windowAvgs[SPLIT] = { 0 };
+	printf("Window %d direction: \n", i);
+	for (int j = 0; j < SPLIT; j++) {
+		for (int k = 0; k < BANDS; k++) {
+			windowAvgs[j] += (fftSubs[SPLIT*i+j].bandAvgs[k] * detectedBands[k]);
+		}
+		windowAvgs[j] = windowAvgs[j] / BANDS;
+		printf(" %f ", windowAvgs[j]);
+	}
+	printf("\n");
+}
+
+// Compare all subwindows, in all bands (and with doppler-ignoring bands)
+// NB: Currently *accumulating* subwindow averages
+void directionSubAllBands(const fft_analysis *fftSubs, const int &i) {
+	double windowAvgs[SPLIT] = { 0 };
+	double windowAvgsTotal = 0;
+	double windowAvgsAccum[SPLIT] = { 0 };
+
+	printf("Window %d direction: \n", i);
+	for (int j = 0; j < SPLIT; j++) {
+		for (int k = 0; k < BANDS; k++) {
+			windowAvgs[j] += (fftSubs[SPLIT*i + j].bandAvgs[k]);
+		}
+		windowAvgs[j] = windowAvgs[j] / BANDS;
+		windowAvgsTotal += windowAvgs[j];
+		windowAvgsAccum[j] = windowAvgsTotal / (j + 1);
+		printf(" %f ", windowAvgs[j]);
+	}
+	printf("\n");
+
+	int dir = 0;
+	
+	for (int j = 1; j < SPLIT; j++) {
+		double rel = windowAvgsAccum[j] / windowAvgsAccum[j - 1];
+		if (rel > (1 + dirMargin)) {
+			dir++;
+		}
+		if (rel < (1 - dirMargin)) {
+			dir--;
+		}		
+	}
+	printf("Detected EV is moving in direction %d \n", dir);
 }
 
 int main()
 {
 	// Read recording
-	rec_data recording = readRecording("police_wail_rec.wav");
+	rec_data recording = readRecording("yt_yelp_approaching.wav");
 
-	// --------------------DETECTION----------------\\
+	// --------------------PARENT----------------\\
 	// Set up Multi-thresholding
 	int nWindow = fullWindow * recording.fs;
-	multiThresholdIndeces mtIndeces = setupMultiThresholding(nWindow, recording.fs);
 
-	fftVars fftV = setupFFT(nWindow); // DO NOT RUN THIS AGAIN
+	multi_thresh_indeces mtIndeces = setupMultiThresholding(nWindow, recording.fs, true);
+
+	fft_vars fftV = setupFFT(nWindow); // DO NOT RUN THIS AGAIN
 
 	// Obtain FFT-analysis
-	fftAnalysis fftAnals[W];
-	int *detectedBands[W];
-
+	fft_analysis fftAnals[W];
+	int detectedBands[W][BANDS];
 	for (int i = 0; i < W; i++) {
 		fftAnals[i] = doFFT(fftV, recording, mtIndeces, nWindow, i);
 		// Detection
-		detectedBands[i] = detect(fftAnals[i]);
+		detect(fftAnals[i], detectedBands[i]);
 	}
 
-	// --------------------DIRECTION----------------\\
+	// Direction
+	directionParentOnly(fftAnals, detectedBands);
+
+	//---------------------SUB---------------------\\
+	
 	// Set up Multi-thresholding
 	int nSubWindow = subWindow * recording.fs;
-	multiThresholdIndeces mtIndecesDir = setupMultiThresholding(nSubWindow, recording.fs);
+	multi_thresh_indeces mtIndecesDir = setupMultiThresholding(nSubWindow, recording.fs, false); // Ignore doppler
 
 	// Obtain FFT-analysis
-	fftAnalysis fftAnalsDir[SW];
-	int *detectedBandsDir[SW];
-
+	fft_analysis fftAnalsDir[SW];
+	int detectedBandsDir[SW][BANDS];
 	for (int i = 0; i < SW; i++) {
 		fftAnalsDir[i] = doFFT(fftV, recording, mtIndecesDir, nSubWindow, i);
-		// Detection
-		detectedBandsDir[i] = detect(fftAnalsDir[i]);
 	}
 
-/*
-	if (check = 1)
-	{
-		// detection direction
-		for (int i = 0; i < SW; i++)
-		{
-			// Obtain absolute, normalised FFT for direction FFT
-			absFFTDirection[i][0] = outDirection[i][0][0] / nWindowDirection;
-			for (long j = 1; j < (nWindowDirection / 2 - 1); j++) {
-				absFFTDirection[i][j] = 2 * sqrt(pow(outDirection[i][j][0], 2) + pow(outDirection[i][j][1], 2)) / nWindowDirection;
-			}
-			// Obtain noise levels for direction
-			double totalNoisedirection = 0;
-			for (long j = noiseIndexLowMin; j < noiseIndexLowMax; j++) {
-				totalNoisedirection += absFFTDirection[i][j];
-			}
-			for (long j = noiseIndexHighMin; j < noiseIndexHighMax; j++) {
-				totalNoisedirection += absFFTDirection[i][j];
-			}
-
-
-			double noiseThreshDirection = noiseMultiplier * totalNoisedirection / ((noiseIndexLowMax - noiseIndexLowMin) + (noiseIndexHighMax - noiseIndexHighMin));
-			// Obtain BoI (Bands of Interest) levels for direction
-			double totalVolDirection[BANDS] = { 0 };
-			int detectionsDirection = 0;
-			for (int j = 0; j < BANDS; j++) {
-				for (int k = bandIndeces[j]; k < bandIndeces[j + 1]; k++) {
-					totalVolDirection[j] += absFFTDirection[i][k];
-				}
-				avgVolDirection[i][j] = totalVolDirection[j] / bandLength;
-				detectionDirection[j] = (avgVolDirection[i][j] >= noiseThreshDirection);
-				detectionsDirection += detectionDirection[j];
-			}
-
-			printf("Window %d: The noise threshold  is %f, the band direction averages are:", i, noiseThreshDirection);
-			for (int j = 0; j < BANDS; j++) {
-				printf(" %f ", avgVolDirection[i][j]);
-			}
-			printf("\n");
-			printf("The siren is present in direction %d out of %d bands \n", detectionsDirection, BANDS);
-			if (detectionsDirection >= (BANDS - 2))
-			{
-				printf("Number of direction deteced bands is greater than or equal %d bands of the original %d bands. \n", BANDS - 2, BANDS);
-
-			}
-		}
+	// Direction
+	for (int i = 0; i < W; i++) {
+		//directionSubParentBands(fftAnalsDir, detectedBands[i], i);
+		directionSubAllBands(fftAnalsDir, i);
 	}
-	*/
+
 	free(recording.samples);    //in is destroyed by plan execution
 	fftw_destroy_plan(fftV.p);
 	fftw_free(fftV.out);
